41b5386ce7671629f13cab4c047f97d5e88a56e3
Jeremia Axel <13519188@std.stei.itb.ac.id>
2023-03-09 19:34:48 +0700

    Create bash script

---
+++ runtram.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+tram_route_file="./custom_scenarios/routes/tram_routes.xml"
+scenarios_file="./custom_scenarios/routes/other_actor_scenarios.json"
+agent_file="./custom_scenarios/autoagents/human_tram_agent.py"
+route_number=$1
+is_debug=$2
+
+(set -x; python scenario_runner.py --route $tram_route_file $scenarios_file $route_number --agent $agent_file $is_debug)
