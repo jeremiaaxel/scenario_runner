accf8336954bcc2e8734ca1ff180e7ee4c27cdd2
Josep Marcello <57025993+jspmarc@users.noreply.github.com>
2023-05-08 13:08:43 +0700

    chore(human tram agent): add no gui support (#2)
    Co-authored-by: Jeremia Axel <13519188@std.stei.itb.ac.id>

--- runtram.sh
+++ runtram.sh
@@ -3,7 +3,11 @@
 tram_route_file="./customs/routes/tram_routes.xml"
 scenarios_file="./customs/routes/full_scenarios.json"
 agent_file="./customs/autoagents/hils_agent.py"
+agent_config_file="./customs/autoagents/configs/human_tram.json"
 route_number=$1
 is_debug=$2
 
-(set -x; python3 scenario_runner.py --route $tram_route_file $scenarios_file $route_number --agent $agent_file $is_debug)
+(set -x; python3 scenario_runner.py \
+            --route $tram_route_file $scenarios_file $route_number \
+            --agent $agent_file $is_debug \
+            --agentConfig $agent_config_file)
