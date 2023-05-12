e41e538f3e28cbfbec8d53b1fe9b39b8fd89575d
Jeremia Axel <13519188@std.stei.itb.ac.id>
2023-05-12 15:57:49 +0700

    runtram with repetition

--- runtram.sh
+++ runtram.sh
@@ -1,13 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 tram_route_file="./customs/routes/tram_routes.xml"
-scenarios_file="./customs/routes/test_scenarios.json"
 agent_file="./customs/autoagents/human_tram_agent.py"
 agent_config_file="./customs/autoagents/configs/human_tram.json"
-route_number=$1
-is_debug=$2
+repetition=$1
+route_number=$2
+is_debug=$3
 
-(set -x; python3 scenario_runner.py \
-            --route $tram_route_file $scenarios_file $route_number \
-            --agent $agent_file $is_debug \
-            --agentConfig $agent_config_file)
+scenarios_file="./customs/routes/test_scenarios.json"
+
+for (( i=1 ; i<=$repetition ; i++ ));
+do
+    echo "#-repetition: $i"
+    (set -x; python3 scenario_runner.py \
+                --route $tram_route_file $scenarios_file $route_number \
+                --agent $agent_file $is_debug \
+                --agentConfig $agent_config_file)
+done
+echo "Done"
